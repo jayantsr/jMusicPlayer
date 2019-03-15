dc1d77bb75387fa0a584dd5d0d7c68df1a7f2a68
Jayan <jayantsr@outlook.com>
2019-03-15 16:04:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jmusicplayer/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.jmusicplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button play, pause, stop;
+    MediaPlayer mediaplayer;
+    int pauseCurrentPosition;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        play = (Button) findViewById(R.id.btn_play);
+        pause = (Button) findViewById(R.id.btn_pause);
+        stop = (Button) findViewById(R.id.btn_stop);
+
+        play.setOnClickListener(this);
+        pause.setOnClickListener(this);
+        stop.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.btn_play:
+                if (mediaplayer==null) {
+                    mediaplayer = MediaPlayer.create(getApplicationContext(), R.raw.blogaudio);
+                    mediaplayer.start();
+                }
+                else if (!mediaplayer.isPlaying()) {
+                    mediaplayer.seekTo(pauseCurrentPosition);
+                    mediaplayer.start();
+                }
+                break;
+
+            case R.id.btn_pause:
+                if (mediaplayer!=null){
+                    mediaplayer.pause();
+                    pauseCurrentPosition = mediaplayer.getCurrentPosition();
+                }
+                break;
+
+            case R.id.btn_stop:
+                if (mediaplayer!=null){
+                    mediaplayer.stop();
+                    mediaplayer = null;
+                }
+                break;
+        }
+    }
+}
